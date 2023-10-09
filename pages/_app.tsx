4c8915ab43a8397e29e318559f34cd0e32c1971b
Juan Lopez <juanlopez@farahs-mbp-2.mynetworksettings.com>
2023-10-09 15:18:47 -0400

    Added Meta Data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,17 @@ import { AppProps } from "next/app";
 import Navbar from "@/components/Navbar"; // Import the Navbar component
 import Footer from "@/components/Footer";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Juan Lopez @Zealthy</title>
+        <meta name="description" content="Juan Lopez Zealthy App" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/dev.png" />
+      </Head>
       <Navbar /> {/* Include the Navbar component */}
       <Component {...pageProps} />
       <Footer />
